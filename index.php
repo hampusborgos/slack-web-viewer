5f93b8d31e192cb61057fc8a8243bc71b0cae412
Hampus Nilsson <mail@hjnilsson.com>
2017-11-28 10:53:08 +0100

    Only live reload in dev environment.

--- index.php
+++ index.php
@@ -385,8 +385,13 @@ if ($_GET['order'] !== 'asc') {
     </script>
     <?php
 }
-?>
 
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
+if ($_SERVER['REQUEST_URI'] == 'localhost') {
+	?>
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+	<?php
+}
+?>
 </body>
 </html>
